4cb01df47bb347d5e959c618b9952721de351ed3
Apprentice <apprentice@dbc04.local>
2012-11-06 17:22:38 -0800

    Finished

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ActiveRecord::Base
+  include TextValidations
+
   has_many :comments, dependent: :destroy
 
   attr_accessible :body, :title
+
+  validates :title, :body, :presence => true
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ActiveRecord::Base
+  include TextValidations
+
   belongs_to :article
 
-  attr_accessible :body
+  attr_accessible :body, :article_id
+
+  validates :body, :length => { :maximum => 250 }
+  validates :article_id, :presence => true
 end
